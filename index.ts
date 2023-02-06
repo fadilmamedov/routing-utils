6d8eee3a1a2aa899473ffbec9669aa039f47bb39
Fadil Mamedov <fadil.mamedov@gobolt.com>
2023-02-06 13:03:54 +0300

    Add command line args

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import ora from "ora";
 import chalk from "chalk";
 import inquirer from "inquirer";
 import clipboardy from "clipboardy";
+import commandLineArgs from "command-line-args";
 import { table } from "table";
 import dayjs from "dayjs";
 import dayjsDuration from "dayjs/plugin/duration";
@@ -24,124 +25,97 @@ import { Route, Location, ServiceArea, RouteAnalysis } from "./types";
 import { getWarehouseLocation } from "./getWarehouseLocation";
 import { getAppointmentLocation } from "./utils";
 
+type Options = {
+  flow: string;
+  serviceArea: string;
+  date: string;
+  route: string;
+};
+const options = commandLineArgs([
+  { name: "flow", alias: "f", type: String },
+  { name: "serviceArea", alias: "s", type: String },
+  { name: "date", alias: "d", type: String },
+  { name: "route", alias: "r", type: String },
+]) as Options;
+
 dayjs.extend(dayjsDuration);
 
 const spinner = ora();
 
-main();
+let authToken: string;
 
-async function main() {
-  let authToken: string;
-
-  if (AUTH_TOKEN === undefined) {
-    console.log("No authentication token provided in environment variables. Please sign in");
-    authToken = await authenticate();
-  } else {
-    authToken = AUTH_TOKEN;
-  }
-  api.defaults.headers["Authorization"] = `Bearer ${authToken}`;
-  silverApi.defaults.headers["Authorization"] = `Bearer ${authToken}`;
-
-  const { flow } = await inquirer.prompt<{ flow: string }>([
-    {
-      name: "flow",
-      message: "Select flow",
-      type: "list",
-      choices: ["GoBolt", "IKEA"],
-    },
-  ]);
-
-  spinner.text = "Fetching service areas...";
-  spinner.start();
-  const serviceAreas = await fetchServiceAreas();
-  spinner.stop();
-
-  let { selectedServiceAreaName } = await inquirer.prompt<{ selectedServiceAreaName: string }>([
-    {
-      name: "selectedServiceAreaName",
-      message: "Select service area",
-      type: "list",
-      default: "yyz",
-      choices: serviceAreas.map((serviceArea) => ({
-        name: getServiceAreaLabel(serviceArea),
-        value: serviceArea.name,
-      })),
-    },
-  ]);
-  const selectedServiceArea = serviceAreas.find((serviceArea) => serviceArea.name === selectedServiceAreaName);
-  if (!selectedServiceArea) return;
-
-  const { selectedDate } = await inquirer.prompt<{ selectedDate: string }>([
-    {
-      name: "selectedDate",
-      message: "Enter date",
-      // FIXME
-      default: "2023-01-20", // dayjs().format("YYYY-MM-DD"),
-    },
-  ]);
-
-  spinner.text = "Fetching routes and appointments...";
-  spinner.start();
-  const [allRoutes, appointments] = await Promise.all([
-    fetchRoutes(selectedDate),
-    fetchAppointments(flow, selectedDate, selectedServiceArea),
-  ]);
-  spinner.stop();
-
-  const appointmentsLib = _.keyBy(appointments, (appt) => appt.id);
-
-  const routes = allRoutes.filter((route) => {
-    return route.route_items.every((routeItem) => appointmentsLib[routeItem.appointment_id]);
-  });
-
-  const { selectedRouteId } = await inquirer.prompt<{ selectedRouteId: string }>([
-    {
-      name: "selectedRouteId",
-      message: "Select route",
-      type: "list",
-      choices: routes.map((route) => ({
-        name: `${getRouteLabel(route)} ${chalk.dim(`[${route.id}]`)}`,
-        value: route.id,
-      })),
-    },
-  ]);
-  const selectedRoute = routes.find((route) => route.id === selectedRouteId);
-  if (!selectedRoute) return;
-
-  const selectedRouteAppointments = selectedRoute.route_items.map(
-    ({ appointment_id }) => appointmentsLib[appointment_id]
-  );
-  const selectedRouteFirstAppointment = selectedRouteAppointments[0];
-
-  const warehouseLocation = getWarehouseLocation(selectedServiceArea.name, selectedRouteFirstAppointment.delivery_type);
-  if (!warehouseLocation) {
-    console.log(chalk.red("Can't find warehouse location"));
-    return;
-  }
-
-  const selectedRouteLocations = selectedRoute.route_items.map((routeItem): Location => {
-    const appointment = appointmentsLib[routeItem.appointment_id];
-    return getAppointmentLocation(appointment);
-  });
-
-  spinner.text = "Fetching directions...";
-  spinner.start();
-  const directions = await fetchDirections([warehouseLocation, ...selectedRouteLocations, warehouseLocation]);
-  spinner.stop();
-
-  outputRouteSummary(selectedRoute, directions);
-
-  spinner.text = "Fetching route analysis...";
-  spinner.start();
-  const routeAnalysis = await fetchRouteAnalysis(selectedRoute);
-  spinner.stop();
-
-  if (!routeAnalysis) {
-    console.log(chalk.red("Can't find route analysis for the selected route"));
-  } else {
-    outputRouteAnalysis(selectedRoute, routeAnalysis);
-  }
+if (AUTH_TOKEN === undefined) {
+  console.log("No authentication token provided in environment variables. Please sign in");
+  authToken = await authenticate();
+} else {
+  authToken = AUTH_TOKEN;
 }
+api.defaults.headers["Authorization"] = `Bearer ${authToken}`;
+silverApi.defaults.headers["Authorization"] = `Bearer ${authToken}`;
+
+const selectedFlow = await promptFlow();
+
+spinner.text = "Fetching service areas...";
+spinner.start();
+const serviceAreas = await fetchServiceAreas();
+spinner.stop();
+
+const selectedServiceArea = await promptServiceArea();
+const selectedDate = await promptDate();
+
+spinner.text = "Fetching routes and appointments...";
+spinner.start();
+const [allRoutes, appointments] = await Promise.all([
+  fetchRoutes(selectedDate),
+  fetchAppointments(selectedFlow, selectedDate, selectedServiceArea),
+]);
+spinner.stop();
+
+const appointmentsLib = _.keyBy(appointments, (appt) => appt.id);
+
+const routes = allRoutes.filter((route) => {
+  return route.route_items.every((routeItem) => appointmentsLib[routeItem.appointment_id]);
+});
+
+const selectedRoute = await promptRoute();
+
+const selectedRouteAppointments = selectedRoute.route_items.map(
+  ({ appointment_id }) => appointmentsLib[appointment_id]
+);
+const selectedRouteFirstAppointment = selectedRouteAppointments[0];
+
+const warehouseLocation = getWarehouseLocation(selectedServiceArea.name, selectedRouteFirstAppointment.delivery_type);
+if (!warehouseLocation) {
+  console.log(chalk.red("Can't find warehouse location"));
+  process.exit(1);
+}
+
+const selectedRouteLocations = selectedRoute.route_items.map((routeItem): Location => {
+  const appointment = appointmentsLib[routeItem.appointment_id];
+  return getAppointmentLocation(appointment);
+});
+
+spinner.text = "Fetching directions...";
+spinner.start();
+const directions = await fetchDirections([warehouseLocation, ...selectedRouteLocations, warehouseLocation]);
+spinner.stop();
+
+outputRouteSummary(selectedRoute, directions);
+
+spinner.text = "Fetching route analysis...";
+spinner.start();
+const routeAnalysis = await fetchRouteAnalysis(selectedRoute);
+spinner.stop();
+
+if (!routeAnalysis) {
+  console.log(chalk.red("Can't find route analysis for the selected route"));
+} else {
+  outputRouteAnalysis(selectedRoute, routeAnalysis);
+}
+
+const command = `npm run main -- -f ${selectedFlow} -s ${selectedServiceArea.name} -d ${selectedDate} -r ${selectedRoute.id}`;
+clipboardy.writeSync(command);
+console.log(`Command: ${chalk.cyan(command)}. Copied to clipboard`);
 
 async function authenticate() {
   while (true) {
@@ -181,12 +155,88 @@ async function authenticate() {
   }
 }
 
-function getRouteLabel(route: Route) {
-  return `R#${route.route_number} - ${route.route_name ?? "No Nickname"}`;
+async function promptFlow() {
+  if (options.flow) {
+    outputQuestionAnswer("Select flow", options.flow);
+    return options.flow;
+  }
+
+  const { flow } = await inquirer.prompt<{ flow: string }>([
+    {
+      name: "flow",
+      message: "Select flow",
+      type: "list",
+      choices: ["GoBolt", "IKEA"],
+    },
+  ]);
+  return flow;
 }
 
-function getServiceAreaLabel(serviceArea: ServiceArea) {
-  return `${serviceArea.humanized_name} ${chalk.dim(_.upperCase(serviceArea.name))}`;
+async function promptServiceArea() {
+  if (options.serviceArea) {
+    const serviceArea = getServiceAreaByName(options.serviceArea);
+    outputQuestionAnswer("Select service area", getServiceAreaLabel(serviceArea));
+    return serviceArea;
+  }
+
+  let { selectedServiceAreaName } = await inquirer.prompt<{ selectedServiceAreaName: string }>([
+    {
+      name: "selectedServiceAreaName",
+      message: "Select service area",
+      type: "list",
+      default: "yyz",
+      choices: serviceAreas.map((serviceArea) => ({
+        name: getServiceAreaLabel(serviceArea),
+        value: serviceArea.name,
+      })),
+    },
+  ]);
+  return getServiceAreaByName(selectedServiceAreaName);
+
+  function getServiceAreaByName(name: string) {
+    return serviceAreas.find((serviceArea) => serviceArea.name === name) as ServiceArea;
+  }
+}
+
+async function promptDate() {
+  if (options.date) {
+    outputQuestionAnswer("Select date", options.date);
+    return options.date;
+  }
+
+  const { selectedDate } = await inquirer.prompt<{ selectedDate: string }>([
+    {
+      name: "selectedDate",
+      message: "Select date",
+      default: dayjs().format("YYYY-MM-DD"),
+    },
+  ]);
+  return selectedDate;
+}
+
+async function promptRoute() {
+  if (options.route) {
+    const route = getRouteById(options.route);
+    outputQuestionAnswer("Select route", `${getRouteLabel(route)} ${chalk.dim(`[${route.id}]`)}`);
+    return route;
+  }
+
+  const { selectedRouteId } = await inquirer.prompt<{ selectedRouteId: string }>([
+    {
+      name: "selectedRouteId",
+      message: "Select route",
+      type: "list",
+      choices: routes.map((route) => ({
+        name: `${getRouteLabel(route)} ${chalk.dim(`[${route.id}]`)}`,
+        value: route.id,
+      })),
+    },
+  ]);
+  return getRouteById(selectedRouteId);
+
+  function getRouteById(id: string) {
+    return routes.find((route) => route.id === id) as Route;
+  }
 }
 
 function outputRouteSummary(route: Route, directions: Leg[]) {
@@ -196,7 +246,6 @@ function outputRouteSummary(route: Route, directions: Leg[]) {
   const totalDurationSeconds = _.sumBy(directions, "duration");
   const totalDuration = dayjs.duration(totalDurationSeconds, "seconds");
 
-  console.log(chalk.blue("Route summary"));
   const output = table(
     [
       [chalk.green("Route"), chalk.green("Total Distance (km)"), chalk.green("Total Duration")],
@@ -205,6 +254,10 @@ function outputRouteSummary(route: Route, directions: Leg[]) {
     {
       columnDefault: {
         alignment: "center",
+      },
+      header: {
+        alignment: "center",
+        content: "Route summary",
       },
     }
   );
@@ -215,7 +268,6 @@ function outputRouteAnalysis(route: Route, routeAnalysis: RouteAnalysis) {
   const totalDistanceKm = routeAnalysis.analysis.total_travel_distance_in_meters / 1000;
   const totalDuration = dayjs.duration(routeAnalysis.analysis.total_duration_in_seconds, "seconds");
 
-  console.log(chalk.blue("Route analysis"));
   const output = table(
     [
       [chalk.green("Route"), chalk.green("Total distance (km)"), chalk.green("Total Duration")],
@@ -225,7 +277,23 @@ function outputRouteAnalysis(route: Route, routeAnalysis: RouteAnalysis) {
       columnDefault: {
         alignment: "center",
       },
+      header: {
+        alignment: "center",
+        content: "Route analysis",
+      },
     }
   );
   console.log(output);
+}
+
+function getRouteLabel(route: Route) {
+  return `R#${route.route_number} - ${route.route_name ?? "No Nickname"}`;
+}
+
+function getServiceAreaLabel(serviceArea: ServiceArea) {
+  return `${serviceArea.humanized_name} ${chalk.dim(_.upperCase(serviceArea.name))}`;
+}
+
+function outputQuestionAnswer(question: string, answer: string) {
+  console.log(chalk.green("?"), question, chalk.cyan(answer));
 }
